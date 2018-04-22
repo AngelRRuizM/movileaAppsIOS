a9d1709ce3dd18dc14002d56900fd9b61588b087
frida manzana <fridamanzana@fridas-macbook-pro.local>
2018-04-22 18:53:49 -0500

    Initial Commit

---
+++ vw/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vw
+//
+//  Created by frida manzana on 4/22/18.
+//  Copyright © 2018 gekko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
